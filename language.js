417620d16d7fea3b2aa282b203b76ca827676401
Gavin <anonymous.user.cn@gmail.com>
2023-07-22 16:47:38 +0800

    1.设置英文你字体；删掉中文地图等。

--- language.js
+++ language.js
@@ -30,19 +30,25 @@ function loadProperties(type) {
         callback: function () { // 回调方法
             $('.i18n').each(function(){
                 $(this).text($.i18n.prop($(this).attr("name")));
+                if(type=="zh_CN") {
+                    $(this).removeAttr("style");
+                } else {
+                    $(this).prop("style", "font-family: 'Calibri Light', arial, sans-sarif;");
+                }
             });
             if (type=="zh_CN") {
                 $('#sec6_slide_file').attr('href', 'file/slides_cn.pptx');
                 $('#sec6_slide_file').attr('download', '公司PPT.pptx');
                 $('#sec6_product_file').attr('href', 'file/products_cn.pdf');
                 $('#sec6_product_file').attr('download', '产品介绍.pdf');
-                $('#sec6_map_img').attr('src', 'img/map_cn.png');
+                // $('#sec6_map_img').attr('src', 'img/map_cn.png');
             } else {
                 $('#sec6_slide_file').attr('href', 'file/slides_en.pptx');
                 $('#sec6_slide_file').attr('download', 'Slide.pptx');
                 $('#sec6_product_file').attr('href', 'file/products_en.pdf');
                 $('#sec6_product_file').attr('download', 'Product catalogue.pdf');
-                $('#sec6_map_img').attr('src', 'img/map_en.png');
+                $('#sec6_address').removeAttr("style");
+                // $('#sec6_map_img').attr('src', 'img/map_en.png');
             }
         }
     });
