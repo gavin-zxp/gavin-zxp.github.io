6b064ef0de7e20d7d49cca7a0014e4348b9da3a7
Gavin <anonymous.user.cn@gmail.com>
2023-10-29 21:02:18 +0800

    完善部分seo相关内容

--- language.js
+++
@@ -1,7 +0,0 @@
-$("#lang_cn").click(function () {
-    window.location.href='index-zh.html#AboutUs';
-});
-
-$("#lang_en").click(function () {
-    window.location.href='index-en.html#AboutUs';
-});
